f4ed265d1c168cce09fec8c57f409142a8ba1019
WaficA <wafic.abass@outlook.com>
2022-12-05 12:42:27 +0000

    .

---
+++ script.js
@@ -0,0 +1,109 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+///////////////////////////////////////
+// Using Google, StackOverflow and MDN
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const tempratures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+// - What is temp amplitude? Answer: difference between highest and lowest temp
+// - How to compute max and min temperatures?
+// - What's a sensor error? And what do do?
+// 2) Breaking up into sub-problems
+// - How to ignore errors?
+// - Find max value in temp array
+// - Find min value in temp array
+// - Subtract min from max (amplitude) and return it
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? NO! Just merge two arrays
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays
+
+function calcTempAmplitude(temp) {
+  let max = temp[0];
+  let min = temp[0];
+  for (let i = 0; i < temp.length; ++i) {
+    const curTemp = temp[i];
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+}
+
+const amplitude = calcTempAmplitude(tempratures);
+console.log(amplitude);
+///////////////////////////////////////
+// Debugging with the Console and Breakpoints
+
+// B) FIND
+
+// A) IDENTIFY
+
+// Using a debugger
+
+// A) IDENTIFY
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+const allData = data1.concat(data2);
+
+function printForcast(arr) {
+  let max = arr[0];
+  let min = arr[0];
+  for (let i = 0; i < arr.length; ++i) {
+    const curTemp = arr[i];
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+}
+
+const amplitude2 = printForcast(allData);
+console.log(amplitude2);
+// 1) Understanding the problem
+// - Array transformed to string, separated by ...
+// - What is the X days? Answer: index + 1
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each element to string with ºC
+// - Strings needs to contain day (index + 1)
+// - Add ... between elements and start and end of string
+// - Log string to console
+
+function mesurekelvin() {
+  const messurment = {
+    type: "temp",
+    unit: "celcius",
+    value: Number(prompt("degrees celcuis")),
+  };
+  console.log(messurment);
+  console.log(typeof messurment.value);
+  //console.warn(messurment);
+  //console.error(messurment);
+  //console.table(messurment);
+  const kelvin = messurment.value + 273;
+  return kelvin;
+}
+console.log(mesurekelvin(101));
